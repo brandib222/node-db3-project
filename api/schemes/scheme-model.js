6937d73696a19677efd4eee575c6125af42bc249
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-17 17:47:12 -0500

    did findById

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -25,7 +25,7 @@ function find() { // EXERCISE A
       .groupBy('sc.scheme_id') 
 }
 
-function findById(scheme_id) { // EXERCISE B
+async function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -91,6 +91,27 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+      const rows = await db('schemes as sc')
+        .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+        .where('sc.scheme_id', scheme_id)
+        // This threw an error, and I don't think it's needed????
+        //.select('st.*, sc.name')
+        .orderBy('st.step_number')
+
+      // const result = {
+      //   scheme_id:rows[0].scheme_id,
+      //   scheme_name:rows[0].scheme_name,
+      //   steps: rows[0]
+      //}
+      const result = {
+        scheme_id: rows[0].scheme_id,
+        scheme_name: rows[0].scheme_name,
+        steps: rows[0].step_number
+      }
+
+      return result;
+
+
 }
 
 function findSteps(scheme_id) { // EXERCISE C
